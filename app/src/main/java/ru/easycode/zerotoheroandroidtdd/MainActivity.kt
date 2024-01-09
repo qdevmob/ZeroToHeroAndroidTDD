86c77b395a6190c3020ae68ec6b6c9d1d8e27fa9
= <=>
2024-01-09 12:21:04 +0300

    task/7 test_keep_text onSaveInstanceState(outState: Bundle)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,15 +6,27 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.changeButton)
-        val textView = findViewById<TextView>(R.id.titleTextView)
+         textView = findViewById(R.id.titleTextView)
 
         button.setOnClickListener {
             textView.text = "I am an Android Developer!"
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key",textView.text.toString())
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text=savedInstanceState.getString("key")
     }
 }
