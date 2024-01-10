e4fb27b0eaa10cb3568cbf82b064670813d5d59a
= <=>
2024-01-10 20:05:07 +0300

    task/11 Count.Base OOP

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.io.Serial
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
+    private val count: Count = Count.Base(step = 2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.countTextView)
+        val button = findViewById<Button>(R.id.incrementButton)
+
+        button.setOnClickListener {
+            text.text = count.increment(number = text.text.toString())
+        }
     }
 }
+
+
+
